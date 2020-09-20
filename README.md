2cb1fb38bcb564ee7f611158df15551725e31b9f
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2020-09-20 10:48:33 -0300

    v.1.0
    PORTUGUÊS
    - Versão final do vídeo no youtube.
    
    ENGLISH
    - Final version of youtube video.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # countdown-timer
- 
+
+PORTUGUÊS
+
+Projeto - Contador regressivo até o ano novo.
+Inspirado no vídeo do youtube ['10 JavaScript Projects in 10 Hours - Coding Challenge - Countdown Timer'](https://youtu.be/dtKciwk_si4?t=72)
+
+ENGLISH
+
+New Year Countdown
+Inspired by youtube video ['10 JavaScript Projects in 10 Hours - Coding Challenge - Countdown Timer'](https://youtu.be/dtKciwk_si4?t=72)
---
+++ script.js
@@ -0,0 +1,26 @@
+const newYear = '1 jan 2021';
+
+function formatTime ( time ) {
+    return time < 10 ? `0${ time }` : time;
+}
+
+function countdown () {
+    const newYearDate = new Date( newYear );
+    const currentDate = new Date();
+
+    const totalSeconds = Math.floor( ( newYearDate - currentDate ) / 1000 );
+
+    const days = parseInt( ( totalSeconds / 3600 ) / 24 );
+    const hours = parseInt( ( totalSeconds / 3600 ) % 24 );
+    const minutes = parseInt( ( totalSeconds / 60 ) % 60 );
+    const seconds = totalSeconds % 60;
+
+    document.getElementById( 'days' ).textContent = days;
+    document.getElementById( 'hours' ).textContent = formatTime( hours );
+    document.getElementById( 'minutes' ).textContent = formatTime( minutes );
+    document.getElementById( 'seconds' ).textContent = formatTime( seconds );
+
+    // console.log( days, hours, minutes, seconds );
+}
+
+setInterval( countdown, 1000 );
